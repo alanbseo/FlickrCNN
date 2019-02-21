1bfaa0e47e795df6c0ede949cff42659ff097293
alanbseo <pompoco78@gmail.com>
2019-02-21 21:41:15 +0100

    Tagging polished

--- SettleData_BatchTagging_using_instadata_v2.py
+++ SettleData_BatchTagging_using_instadata_v2.py
@@ -152,6 +152,110 @@ filename = 'photoid_23663993529.jpg' # bridge
 
 
 
+
+for filename in filenames[1983:]:
+
+
+    fname = photo_path + "/" + filename
+
+    if os.path.isfile(fname):
+
+        # load an image in PIL format
+        # original = load_img(filename, target_size=(299, 299))
+        try:
+            original = load_img(fname, target_size=(662, 662))
+        except OSError:
+            print("Bad file - Try again..." + fname)
+            continue
+
+        # convert the PIL image to a numpy array
+        # IN PIL - image is in (width, height, channel)
+        # In Numpy - image is in (height, width, channel)
+        numpy_image = img_to_array(original)
+
+        # Convert the image / images into batch format
+        # expand_dims will add an extra dimension to the data at a particular axis
+        # We want the input matrix to the network to be of the form (batchsize, height, width, channels)
+        # Thus we add the extra dimension to the axis 0.
+        image_batch = np.expand_dims(numpy_image, axis=0)
+
+
+        # prepare the image (normalisation for channels)
+        processed_image = inception_resnet_v2.preprocess_input(image_batch.copy())
+
+        # get the predicted probabilities for each class
+        predictions = model_trained.predict(processed_image)
+
+        # print predictions
+        dominant_feature_idx = np.argmax(predictions[0])
+
+        # This is the dominant entry in the prediction vector
+        dominant_output = model_trained.output[:, dominant_feature_idx]
+
+
+        # convert the probabilities to class labels
+        predicted_class = classes[dominant_feature_idx]
+        print('Predicted:', predicted_class)
+
+        # The is the output feature map of the `conv_7b_ac` layer,
+        # the last convolutional layer in InceptionResnetV2
+        last_conv_layer = model_trained.get_layer('conv_7b_ac')
+
+        # This is the gradient of the dominant class with regard to
+        # the output feature map of `conv_7b_ac`
+        grads = k.gradients(dominant_output, last_conv_layer.output)[0]
+
+        # This is a vector of shape (1536,), where each entry
+        # is the mean intensity of the gradient over a specific feature map channel
+        pooled_grads = k.mean(grads, axis=(0, 1, 2))
+
+        # This function allows us to access the values of the quantities we just defined:
+        # `pooled_grads` and the output feature map of `conv_7b_ac`,
+        # given a sample image
+        iterate = k.function([model_trained.input], [pooled_grads, last_conv_layer.output[0]])
+
+        # These are the values of these two quantities, as Numpy arrays,
+        # given our sample image of two elephants
+        pooled_grads_value, conv_layer_output_value = iterate([processed_image])
+
+        # We multiply each channel in the feature map array
+        # by "how important this channel is" with regard to the elephant class
+        for i in range(1536):
+            conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+        # The channel-wise mean of the resulting feature map
+        # is our heatmap of class activation
+        heatmap = np.mean(conv_layer_output_value, axis=-1)
+
+
+        # For visualization purpose, we will also normalize the heatmap between 0 and 1:
+
+        heatmap = np.maximum(heatmap, 0)
+        heatmap /= np.max(heatmap)
+        # plt.matshow(heatmap)
+        # plt.show()
+
+
+        # We use cv2 to load the original image
+        img = cv2.imread(fname)
+
+        # We resize the heatmap to have the same size as the original image
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+
+        # We convert the heatmap to RGB
+        heatmap = np.uint8(255 * heatmap)
+
+        # We apply the heatmap to the original image
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+        # 0.4 here is a heatmap intensity factor
+        superimposed_img = heatmap * 0.4 + img
+
+        # Save the image to disk
+        cv2.imwrite("Result/Heatmap_" + modelname + "/AttentionMap_" + predicted_class + "_" + filename, superimposed_img)
+
+
+
 for filename in filenames[:]:
 
 
@@ -197,8 +301,7 @@ for filename in filenames[:]:
 
 
         df = pd.DataFrame(predictions[0]).transpose()
-        name_csv = default_path + "/Result/Tag_" + modelname + "/" + filename + ".csv"
-
+        name_csv = default_path + "/Result/Tag_" + modelname + "/" + filename + "_" + predicted_class + ".csv"
 
 
         # df.to_csv(name_csv)
@@ -207,114 +310,5 @@ for filename in filenames[:]:
         df.to_csv(name_csv, index=False, columns= header)
 
 
-
-
-
-for filename in filenames[2000:]:
-
-
-    fname = photo_path + "/" + filename
-
-    if os.path.isfile(fname):
-
-        # load an image in PIL format
-        # original = load_img(filename, target_size=(299, 299))
-        try:
-            original = load_img(fname, target_size=(662, 662))
-        except OSError:
-            print("Bad file - Try again..." + fname)
-            continue
-
-        # convert the PIL image to a numpy array
-        # IN PIL - image is in (width, height, channel)
-        # In Numpy - image is in (height, width, channel)
-        numpy_image = img_to_array(original)
-
-        # Convert the image / images into batch format
-        # expand_dims will add an extra dimension to the data at a particular axis
-        # We want the input matrix to the network to be of the form (batchsize, height, width, channels)
-        # Thus we add the extra dimension to the axis 0.
-        image_batch = np.expand_dims(numpy_image, axis=0)
-
-
-
-        # prepare the image (normalisation for channels)
-        processed_image = inception_resnet_v2.preprocess_input(image_batch.copy())
-
-
-
-        # get the predicted probabilities for each class
-        predictions = model_trained.predict(processed_image)
-
-
-        # print predictions
-        dominant_feature_idx = np.argmax(predictions[0])
-
-
-
-        # This is the dominant entry in the prediction vector
-        dominant_output = model_trained.output[:, dominant_feature_idx]
-
-
-        # convert the probabilities to class labels
-        predicted_class = classes[dominant_feature_idx]
-        print('Predicted:', predicted_class)
-
-        # The is the output feature map of the `block5_conv3` layer,
-        # the last convolutional layer in VGG16
-        last_conv_layer = model_trained.get_layer('conv_7b_ac')
-
-        # This is the gradient of the dominant class with regard to
-        # the output feature map of `block5_conv3`
-        grads = k.gradients(dominant_output, last_conv_layer.output)[0]
-
-        # This is a vector of shape (512,), where each entry
-        # is the mean intensity of the gradient over a specific feature map channel
-        pooled_grads = k.mean(grads, axis=(0, 1, 2))
-
-        # This function allows us to access the values of the quantities we just defined:
-        # `pooled_grads` and the output feature map of `block5_conv3`,
-        # given a sample image
-        iterate = k.function([model_trained.input], [pooled_grads, last_conv_layer.output[0]])
-
-        # These are the values of these two quantities, as Numpy arrays,
-        # given our sample image of two elephants
-        pooled_grads_value, conv_layer_output_value = iterate([processed_image])
-
-        # We multiply each channel in the feature map array
-        # by "how important this channel is" with regard to the elephant class
-        for i in range(1536):
-            conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
-
-        # The channel-wise mean of the resulting feature map
-        # is our heatmap of class activation
-        heatmap = np.mean(conv_layer_output_value, axis=-1)
-
-
-        # For visualization purpose, we will also normalize the heatmap between 0 and 1:
-
-        heatmap = np.maximum(heatmap, 0)
-        heatmap /= np.max(heatmap)
-        # plt.matshow(heatmap)
-        # plt.show()
-
-
-        # We use cv2 to load the original image
-        img = cv2.imread(fname)
-
-        # We resize the heatmap to have the same size as the original image
-        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-
-        # We convert the heatmap to RGB
-        heatmap = np.uint8(255 * heatmap)
-
-        # We apply the heatmap to the original image
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
-        # 0.4 here is a heatmap intensity factor
-        superimposed_img = heatmap * 0.4 + img
-
-        # Save the image to disk
-        cv2.imwrite("Result/Heatmap_" + modelname + "/AttentionMap_" + predicted_class + "_" + filename, superimposed_img)
 
 
